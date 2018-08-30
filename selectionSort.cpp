6fd7b542589e72a6a37f7ca314ebd4758a55bd48
Adam <micro.83@hotmail.com>
2018-08-30 16:40:50 +0300

    Create selectionSort.cpp

---
+++ selectionSort.cpp
@@ -0,0 +1,41 @@
+//g++  5.4.0
+
+#include <iostream>
+using namespace std;
+
+void selectionSort (int array[], int length){
+    
+        for (int i = 0; i<length; i++){
+            int min = i;
+            
+            
+            for (int j = i + 1; j < length; j++){
+                if (array[j] < array[min]){
+                    min = j;
+                }
+                
+                
+            }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+            }
+             for (int k = 0; k< length;k++){
+        
+                 cout << array[k] <<endl;
+        }
+
+        }
+         
+    
+    
+      
+   
+int main()
+{
+       int array[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        
+       int length = (sizeof(array)/ sizeof(array[0]));
+       selectionSort(array,length);
+    
+}
