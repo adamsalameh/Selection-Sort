1562468106a35bd0c38cc0a2b0ba43e81ac3a00e
Adam <micro.83@hotmail.com>
2018-08-31 12:20:28 +0300

    Update selectionSort.php

--- selectionSort.php
+++ selectionSort.php
@@ -3,8 +3,11 @@
         //                0  1    2   3   4   5   6   7   8   9  10  11  12  13  14  15 16 17 18 19  20  21  22  23  24
         $numbers = array(29, 31, 37, 41, 79, 83, 89, 43, 47, 53, 59, 61, 67, 71, 73, 2, 3, 5, 7, 11, 13, 17, 19, 23, 97);
        
-        function selectionSort($array) {
-            
+        function selectionSort($array) { 
+         // passing the array by reference will effect the orginal array add & : selectionSort( &$array)
+         // function selectionSort(&$array) {       
+                
+                
         for ($i = 0; $i < count($array); $i++) {
             
             $min = $i;
@@ -25,6 +28,7 @@
     print_r($numbers);
     echo "The array after the selection sort\n";
     print_r(selectionSort($numbers));
+    print_r($numbers); // The orginal array didn't effect if you want to change it pass it by reference 
   
 
 ?>
